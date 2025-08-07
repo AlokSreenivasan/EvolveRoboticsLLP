d45b1acb150d9565436d2f9b4c5f4d59db732921
AlokSreenivasan <aloksreenivasank@gmail.com>
2025-08-07 14:25:03 +0530

    Sign Up Screen w/o error validation

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,23 +1,78 @@
 import React from 'react';
-import {Text, View, StyleSheet, TextInput} from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  ScrollView,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 function SignUpScreen() {
+  const [fullName, setFullName] = React.useState('');
   const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirmPassword, setConfirmPassword] = React.useState('');
   return (
-    <View style={styles.container}>
-      <Text>Sign Up</Text>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
 
-      <TextInput
-        placeholder="Enter your email"
-        placeholderTextColor="#a84fa1"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        style={styles.input}
-      />
+      <View style={styles.card}>
+        <Text style={styles.heading}>Create Your Account</Text>
 
-    </View>
+        <Text style={styles.label}>Full name:</Text>
+        <TextInput
+          placeholder="Enter full name"
+          value={fullName}
+          onChangeText={setFullName}
+          style={styles.input}
+        />
+        {/*{errors.fullName && <Text style={styles.error}>{errors.fullName</Text>}*/}
+
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="your.email@example.com"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        {/*{errors.email && <Text style={styles.error}>{errors.email}</Text>}*/}
+
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Minimum 8 characters"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        {/*{errors.password && <Text style={styles.error}>{errors.password}</Text>}*/}
+
+        <Text style={styles.label}>Confirm Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Re-enter password"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+        />
+        {/*{errors.confirmPassword && (*/}
+        {/*  <Text style={styles.error}>{errors.confirmPassword}</Text>*/}
+        {/*)}*/}
+
+        <TouchableOpacity
+          style={styles.button}
+          // onPress={handleSignUp}
+        >
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.footerText}>
+          Already have an account? <Text style={styles.link}>Sign In</Text>
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -27,15 +82,59 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#fff',
   },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    alignSelf: 'center',
+    padding: 30,
+    marginVertical: 16,
+  },
+  card: {
+    backgroundColor: '#fafafa',
+    borderRadius: 10,
+    padding: 20,
+    elevation: 2,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: '700',
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  label: {
+    marginTop: 36,
+    fontWeight: '500',
+  },
   input: {
     borderWidth: 1,
-    borderColor: '#eecdf4',
-    borderRadius: 15,
-    paddingHorizontal: 15,
-    paddingVertical: 12,
-    fontSize: 16,
-    color: '#000',
-    marginBottom: 15,
+    borderColor: '#ddd',
+    borderRadius: 6,
+    padding: 10,
+    marginTop: 5,
+  },
+  error: {
+    fontSize: 12,
+    color: 'red',
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: '#a42a8b',
+    padding: 15,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+  footerText: {
+    alignSelf: 'center',
+    marginTop: 15,
+  },
+  link: {
+    color: '#a42a8b',
+    fontWeight: '600',
   },
 });
 export default SignUpScreen;
