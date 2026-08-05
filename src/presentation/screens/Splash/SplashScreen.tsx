2f402fb7560bc33192e5556f6880b0d648755908
AlokSreenivasan <aloksreenivasank@gmail.com>
2026-08-05 18:09:14 +0530

    Updated Splash screen

--- src/presentation/screens/Splash/SplashScreen.tsx
+++ src/presentation/screens/Splash/SplashScreen.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { ActivityIndicator, Image, SafeAreaView, StyleSheet, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet } from 'react-native';
 
-import { cardShadowLight, colors, spacing } from '../../../constants/theme';
+import { colors } from '../../../constants/theme';
 
 const SplashScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.logoGlow} />
       <Image
         source={require('../../../assets/LOGO__.png')}
         style={styles.logo}
         resizeMode="contain"
       />
-      <ActivityIndicator size="large" color={colors.primary} style={styles.loader} />
     </SafeAreaView>
   );
 };
@@ -22,24 +20,12 @@ export default SplashScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.primaryLight,
+    backgroundColor: colors.surface,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  logoGlow: {
-    position: 'absolute',
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    backgroundColor: colors.primaryMuted,
-    opacity: 0.45,
-    ...cardShadowLight,
   },
   logo: {
     width: 140,
     height: 140,
-  },
-  loader: {
-    marginTop: spacing.sectionGap,
   },
 });
